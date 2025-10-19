d66075907fa0438651cf7a28eb33501fdb22572d
Jayda <h2awc@unb.ca>
2025-10-19 17:56:31 -0300

    Added basic app functionality and a bottom navigation menu  bar

---
+++ app/app/src/main/java/ca/unb/mobiledev/cookiestepper/StepTracker.kt
@@ -0,0 +1,49 @@
+package ca.unb.mobiledev.cookiestepper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import ca.unb.mobiledev.cookiestepper.databinding.ActivityMainBinding
+import ca.unb.mobiledev.cookiestepper.databinding.ActivityStepTrackerBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class StepTracker : AppCompatActivity() {
+
+    private lateinit var binding : ActivityStepTrackerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityStepTrackerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Steps())
+
+
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.menuSteps -> replaceFragment(Steps())
+                R.id.menuCalories -> replaceFragment(Calories())
+                R.id.menuBMI -> replaceFragment(BMI())
+                R.id.menuRewards -> replaceFragment(Rewards())
+                R.id.menuHistory -> replaceFragment(History())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
